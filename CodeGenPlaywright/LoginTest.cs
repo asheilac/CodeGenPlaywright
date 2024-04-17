7f887d1d6db1576b516ddd8ffdbee46c66ea07c9
Sheila Coles <sheila.coles@ukho.gov.uk>
2024-04-17 14:20:55 +0100

    first login test

--- CodeGenPlaywright/LoginTest.cs
+++ CodeGenPlaywright/LoginTest.cs
@@ -1,15 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
 namespace CodeGenPlaywright;
 
-public class Tests
+[TestFixture]
+public class LoginTests : PageTest
 {
-    [SetUp]
-    public void Setup()
+    private IConfiguration? _testSettings;
+    
+    [OneTimeSetUp]
+    public void OneTimeSetup()
     {
+        _testSettings = new ConfigurationBuilder().AddJsonFile("testSettings.json").Build();
     }
-
-    [Test]
-    public void Test1()
+    
+    [TestCase]
+    public async Task LoginTest()
     {
-        Assert.Pass();
+        ArgumentNullException.ThrowIfNull(_testSettings);
+        await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        
+        var baseUrl = _testSettings["baseUrl"] ?? throw new NullReferenceException("baseUrl is not defined in testsettings.json");
+        var emailAddress = _testSettings["emailAddress"] ?? throw new NullReferenceException("emailAddress is not defined in testsettings.json");
+        var password = _testSettings["password"] ?? throw new NullReferenceException("password is not defined in testsettings.json");
+        
+        await page.GotoAsync(baseUrl);
+        
+        await page.GetByRole(AriaRole.Link, new() { Name = " Signup / Login" }).ClickAsync();
+        await page.Locator("form").Filter(new() { HasText = "Login" }).GetByPlaceholder("Email Address").FillAsync(emailAddress);
+        await page.GetByPlaceholder("Password").FillAsync(password);
+        await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
+        await Expect(page.GetByText("Logged in as Yumi Cheng")).ToBeVisibleAsync();
+
     }
 }
