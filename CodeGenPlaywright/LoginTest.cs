8a8fb13fbddf2ccff9e6661d4aabd0922453c8e6
Sheila Coles <sheila.coles@ukho.gov.uk>
2024-04-17 13:33:09 +0100

    initial commit

---
+++ CodeGenPlaywright/LoginTest.cs
@@ -0,0 +1,15 @@
+namespace CodeGenPlaywright;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
