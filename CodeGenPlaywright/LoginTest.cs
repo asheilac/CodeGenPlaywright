605ae4bfbc8a93091ecb348edb0e818e201f4c92
Sheila Coles <sheila.coles@ukho.gov.uk>
2024-04-24 11:42:00 +0100

    added test for unsuccessful login

--- CodeGenPlaywright/LoginTest.cs
+++ CodeGenPlaywright/LoginTest.cs
@@ -16,7 +16,7 @@ public class LoginTests : PageTest
     }
     
     [TestCase]
-    public async Task LoginTest()
+    public async Task SuccessfulLoginTest()
     {
         ArgumentNullException.ThrowIfNull(_testSettings);
         await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
@@ -34,6 +34,25 @@ public class LoginTests : PageTest
         await page.GetByPlaceholder("Password").FillAsync(password);
         await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
         await Expect(page.GetByText("Logged in as Yumi Cheng")).ToBeVisibleAsync();
+    }
+    
+    [TestCase]
+    public async Task UnsuccessfulLoginTest()
+    {
+        ArgumentNullException.ThrowIfNull(_testSettings);
+        await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        
+        var baseUrl = _testSettings["baseUrl"] ?? throw new NullReferenceException("baseUrl is not defined in testsettings.json");
+        
+        await page.GotoAsync(baseUrl);
+        
+        await page.GetByRole(AriaRole.Link, new() { Name = " Signup / Login" }).ClickAsync();
+        await page.Locator("form").Filter(new() { HasText = "Login" }).GetByPlaceholder("Email Address").FillAsync("e@e");
+        await page.GetByPlaceholder("Password").FillAsync("password");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Login" }).ClickAsync();
 
+        await Expect(page.Locator("#form")).ToContainTextAsync("Your email or password is incorrect!");
     }
 }
