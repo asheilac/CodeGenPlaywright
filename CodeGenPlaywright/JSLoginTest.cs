22b6de114b2482243f22201d669c86997aebdcf0
Sheila Coles <sheila.coles@ukho.gov.uk>
2024-04-26 14:30:34 +0100

    unsuccessful login test and sqlinjection login test

--- CodeGenPlaywright/JSLoginTest.cs
+++ CodeGenPlaywright/JSLoginTest.cs
@@ -28,7 +28,7 @@ public class JSLoginTest : PageTest
         var emailAddress = _testSettings["emailAddress"] ?? throw new NullReferenceException("emailAddress is not defined in testsettings.json");
         var password = _testSettings["password"] ?? throw new NullReferenceException("password is not defined in testsettings.json");
         
-        await page.GotoAsync(baseUrl + "login");
+        await page.GotoAsync($"{baseUrl}login");
         
         await page.GetByLabel("Close Welcome Banner").ClickAsync();
         await page.GetByLabel("Text field for the login email").FillAsync(emailAddress);
@@ -36,5 +36,43 @@ public class JSLoginTest : PageTest
         await page.GetByLabel("Login", new() { Exact = true }).ClickAsync();
         await page.GetByLabel("Show/hide account menu").ClickAsync();
         await Expect(page.Locator("#mat-menu-panel-0")).ToContainTextAsync("yumi@hello.com");
+    }
+    
+    [TestCase]
+    public async Task UnsuccessfulLoginTest()
+    {
+        ArgumentNullException.ThrowIfNull(_testSettings);
+        await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        
+        var baseUrl = _testSettings["baseUrl"] ?? throw new NullReferenceException("baseUrl is not defined in testsettings.json");
+        
+        await page.GotoAsync($"{baseUrl}login");
+
+        await page.GetByLabel("Close Welcome Banner").ClickAsync();
+        await page.GetByLabel("Text field for the login email").FillAsync("who");
+        await page.GetByLabel("Text field for the login password").FillAsync("who");
+        await page.GetByLabel("Login", new() { Exact = true }).ClickAsync();
+        await Expect(page.Locator("mat-card")).ToContainTextAsync("Invalid email or password.");
+    }
+    
+    [TestCase]
+    public async Task UnsuccessfulSQLInjectionLoginTest()
+    {
+        ArgumentNullException.ThrowIfNull(_testSettings);
+        await using var browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions {Headless = false});
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+        
+        var baseUrl = _testSettings["baseUrl"] ?? throw new NullReferenceException("baseUrl is not defined in testsettings.json");
+        
+        await page.GotoAsync($"{baseUrl}login");
+
+        await page.GetByLabel("Close Welcome Banner").ClickAsync();
+        await page.GetByLabel("Text field for the login email").FillAsync("' or 1=1 --");
+        await page.GetByLabel("Text field for the login password").FillAsync("anything");
+        await page.GetByLabel("Login", new() { Exact = true }).ClickAsync();
+        await Expect(page.Locator("mat-card")).ToContainTextAsync("Invalid email or password.");
     }
 }
